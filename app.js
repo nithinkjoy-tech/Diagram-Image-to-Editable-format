4b8ec65fab1b763e36029b7b84915edd6abb5737
Nithin K Joy <nithinjoy121212@gmail.com>
2023-02-07 15:25:04 +0530

    changed add shape logic to another module

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const PPTX = require("nodejs-pptx");
 let pptx = new PPTX.Composer();
 const {spawn} = require("child_process");
-const { execFile } = require('child_process');
+const addShape=require("./addShape")
 
 shapeslist = JSON.parse(process.argv[2]);
 inputimagename = process.argv[3];
 finalshape = [];
-
 
 for (shape of shapeslist) {
   cx = shape[3];
@@ -18,19 +17,10 @@ for (shape of shapeslist) {
 }
 
 const run = async () => {
-  await pptx.compose(async pres => {
-    await pres.addSlide(slide => {
-      finalshape.forEach((_, i) => {
-        shapename = "PPTX.ShapeTypes." + _[0];
-        slide.addShape(shape => {
-          shape.type(eval(shapename)).x(_[1]).y(_[2]).cx(_[3]).cy(_[4]).color("ffffff");
-        });
-      });
-    });
-  });
+  addShape.addShape(finalshape)
 
-  await pptx.save(`./shapes-test.pptx`);
   process = spawn("python", ["./addBorder.py", `${finalshape.length}`, inputimagename]);
+
   process.stdout.on("data", async data => {
     textData = eval(data.toString());
     await pptx.load(`./shapes-test.pptx`);
