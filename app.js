e4a9350d2dd8af9f4891ceaa9eca8d85c2189abc
Nithin K Joy <nithinjoy121212@gmail.com>
2023-02-08 11:45:06 +0530

    seperated extract text and adding border logic to different files

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const addText = require("./addText");
 shapeslist = JSON.parse(process.argv[2]);
 inputimagename = process.argv[3];
 
-
 addShape.addShape(shapeslist);
+shapecount=addShape.shapeCount()
 
-process = spawn("python", ["./addBorder.py", `${finalshape.length}`, inputimagename]);
+process = spawn("python", ["./addBorder.py", `${shapecount}`, inputimagename]);
+
+process = spawn("python", ["./extractText.py", inputimagename]);
 
 process.stdout.on("data", async data => {
   textData = eval(data.toString());
